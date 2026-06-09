f0f7bd436d2473a5c9046d4aac685af84ef4c288
NidhuN <100463732+Nidhun-vp@users.noreply.github.com>
2026-06-09 10:20:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,39 +7,40 @@ dotenv.config();
 
 const app = express();
 
-
 // ROUTES
 const authRoutes = require("./routes/authRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 
-
 // MIDDLEWARE
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "https://job-search-app-nine-xi.vercel.app/",
+    credentials: true,
+  })
+);
 
+app.use(express.json());
 
 // API ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
 
-
 // DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
-  console.log("MongoDB Connected");
-})
-.catch((err) => {
-  console.log(err);
-});
-
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Server Running");
 });
-
 
 // SERVER
 const PORT = process.env.PORT || 5000;
